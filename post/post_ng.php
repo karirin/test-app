eecb943970d059ca08d637a044cab4ec72878cbc
karirin <karirin419@gmail.com>
2020-09-22 19:43:44 +0900

    test

--- post/post_ng.php
+++
@@ -1,4 +0,0 @@
-<body>
-    商品が選択されていません。<br />
-    <a href="post_list.php">戻る</a>
-</body>
--- post_list.php
+++
@@ -1,6 +0,0 @@
-<?php foreach($posts as $post): ?>
-
-    <?php print'<br />'; ?>
-    <?php print ''.$post['name'].''; ?>
-
-<?php endforeach ?>
