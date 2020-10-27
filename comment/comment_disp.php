f2d7859226d500b3c2f3965daee05aaa08fa6a44
karirin <karirin419@gmail.com>
2020-10-27 20:05:06 +0900

    コメント欄のレイアウトアップデート

--- comment/comment_disp.php
+++ comment/comment_disp.php
@@ -7,7 +7,91 @@ require_once('../header.php');
 $post_id=$_GET['post_id'];
 $comment_id=$_GET['comment_id'];
 
+$post = get_post($post_id);
+$post_user = get_user($post['user_id']); 
+$current_user = get_user($_SESSION['user_id']);
+
 print'<div class="reply">';
+print'<div class="col-8 offset-2">';
+print'<div class="post">';
+print'<div class="post_list">';
+print'<div class="post_user">';
+print'<object><a href="/user/user_disp.php?user_id='.$current_user['id'].'&page_id='.$post_user['id'].'">';
+print'<img src="/user/image/'.$post_user['image'].'">'; 
+print''.$post_user['name'].'';
+print'</a></object>';
+print'</div>';
+print'<div class="post_text">';
+print''.$post['text'].'';
+print'</div>';
+
+if (!empty($post['gazou'])):
+print'<img src="/post/gazou/'.$post['gazou'].'" class="post_img" >';
+endif;
+?>
+<div class="post_info">
+<form class="favorite_count" action="#" method="post">
+        <input type="hidden" name="post_id" value="<?= $post['id'] ?>">
+        <button type="button" name="favorite" class="btn favorite_btn" >
+        <?php if (!check_favolite_duplicate($_SESSION['user_id'],$post['id'])): ?>
+          <i class="far fa-star"></i>
+        <?php else: ?>
+          <i class="fas fa-star"></i>
+        <?php endif; ?>
+        </button>
+        <span class="post_count"><?= current(get_post_favorite_count($post['id'])) ?></span>
+</form>
+<div class="post_comment_count">
+<button class="btn modal_btn" data-target="#modal<?= $post['id'] ?>" type="button"><i class="fas fa-comment-dots"></i></button>
+<span class="post_comment_count"><?= current(get_post_comment_count($post['id'])) ?></span>
+</div>
+<div class="comment_confirmation" id="modal<?= $post['id'] ?>">
+            <p class="modal_title" >この投稿にコメントしますか？</p>
+            <p class="post_content"><?= nl2br($post['text']) ?></p>
+            <form method="post" action="../comment/comment_add_done.php" enctype="multipart/form-data">
+              <p>コメント内容を入力ください。</p>
+              <input type="text" name="text">
+              <p>画像を選んでください。</p>
+              <input type="file" name="image_name">
+              <input type="hidden" name="id" value="<?= $post_id ?>">
+              <button class="btn btn-outline-danger" type="submit" name="comment" value="comment">コメント</button>
+              <button class="btn btn-outline-primary modal_close" type="button">キャンセル</button>
+            </form>
+</div>
+<button class="btn modal_btn" data-target="#edit_modal<?= $post['id'] ?>" type="button"><i class="fas fa-edit"></i></button>
+<div class="post_edit" id="edit_modal<?= $post['id'] ?>">
+投稿内容更新
+<form method="post" action="../post/post_edit_done.php" enctype="multipart/form-data">
+投稿内容を編集する
+<input type="text" name="text" value="<?php print $post['text']; ?>">
+<?php
+if(!empty($disp)){
+print $disp_gazou;
+}
+?>
+画像を選んでください<br />
+<input type="file" name="gazou_name" style="width:400px">
+<input type="hidden" name="id" value="<?php print $post['id']; ?>">
+<input type="hidden" name="gazou_name_old" value="<?php print $post['gazou']; ?>">
+<button class="btn btn-outline-danger" type="submit" name="edit" value="edit">更新</button>
+<button class="btn btn-outline-primary modal_close" type="button">キャンセル</button>
+</form>
+</div>
+<button class="btn modal_btn" data-target="#delete_modal<?= $post['id'] ?>" type="button"><i class="far fa-trash-alt"></i></button>
+<div class="delete_confirmation" id="delete_modal<?= $post['id'] ?>">
+            <p class="modal_title" >こちらの投稿を削除しますか？</p>
+            <p class="post_content"><?= nl2br($post['text']) ?></p>
+            <form action="post_delete_done.php" method="post">
+              <input type="hidden" name="id" value="<?= $post['id']?>">
+              <input type="hidden" name="gazou_name" value="<?= $post['gazou']?>">
+              <button class="btn btn-outline-danger" type="submit" name="delete" value="delete">削除</button>
+              <button class="btn btn-outline-primary modal_close" type="button">キャンセル</button>
+            </form>
+</div>
+</div>
+<p class="post_created_at"><?php print''.convert_to_fuzzy_time($post['created_at']).''; ?></p>
+<?php
+
 $reply_comments = get_reply_comments($post_id,$comment_id);
 foreach($reply_comments as $reply_comment):
 if($reply_comment['comment_id']==$comment_id){
@@ -58,4 +142,9 @@ if($reply_comment['comment_id']==$comment_id){
 }
 endforeach;
 print'</div>';
+print'</div>';
 ?>
+</div>
+</div>
+</div>
+<?php require_once('../footer.php');?>
