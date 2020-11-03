13474d65daba73a275ce456d45e753733cd6f9f3
karirin <karirin419@gmail.com>
2020-11-03 12:37:54 +0900

    投稿内容を自動で改行するよう修正

--- post_process.php
+++ post_process.php
@@ -2,7 +2,7 @@
 <div class="post_process">
   <h2>投稿</h2>
   <form method="post" action="../post/post_add_done.php" enctype="multipart/form-data">
-  <textarea class="textarea form-control" placeholder="投稿内容を入力ください" name="content"></textarea>
+  <textarea class="textarea form-control" placeholder="投稿内容を入力ください" name="text"></textarea>
   画像を選んでください。
   <input type="file" name="gazou_name" style="width:200px">
   <button class="btn btn-outline-danger" type="submit" name="post" value="post">コメント</button>
