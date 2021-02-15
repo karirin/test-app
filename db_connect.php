75082a8f4895ad07b2c2115c5d250618000551f4
karirin <karirin419@gmail.com>
2021-02-16 06:49:35 +0900

    db_connect.php作成

---
+++ db_connect.php
@@ -0,0 +1,11 @@
+<?php 
+function dbConnect()
+{
+$dsn = 'mysql:dbname=db;host=localhost;charset=utf8';
+$user = 'root';
+$password = '';
+$dbh = new PDO($dsn,$user,$password);
+$dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+return $dbh;
+}
+?>
