aaeddb0c795961e80df426028c2ccd2d57a4a9ff
karirin <karirin419@gmail.com>
2021-05-01 13:03:02 +0900

    heroku deploy

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,15 @@
 <?php 
 function dbConnect()
 {
-  $dsn = 'mysql:dbname=db;host=localhost;charset=utf8';
-  $user = 'root';
-  $password = '';
-  $dbh = new PDO($dsn,$user,$password);
+  $dsn = 'mysql:dbname=heroku_91d4d2da72d9c6e;host=us-cdbr-east-03.cleardb.com;charset=utf8';
+  $user = 'b2eddc9d906427';
+  $password = 'b7d1bed6';
+  $option = array(  
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+  );
+  $dbh = new PDO($dsn, $user, $password, $option);
   $dbh -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $dbh;
 }
