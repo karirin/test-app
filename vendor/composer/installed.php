8691066366d9b3b4f4a1903160ca8298dfd940cb
karirin <karirin419@gmail.com>
2021-05-02 14:04:43 +0900

    heroku deploy

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3edefbbdf7c576cf3ede27daf03777a7c6c46414',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3edefbbdf7c576cf3ede27daf03777a7c6c46414',
+    ),
+  ),
+);
