e34913316ab34b20cf6e0a00fc31e9367060a786
karirin <karirin419@gmail.com>
2022-05-02 02:30:26 +0900

    '再度db修正'

--- db_connect.php
+++ db_connect.php
@@ -1,19 +1,19 @@
 <?php
 function db_connect()
 {
-  //   if ($_SERVER["HTTP_HOST"] == "localhost") {
-  //     $dsn = 'mysql:dbname=chat;host=localhost;charset=utf8';
-  //     $user = 'root';
-  //     $password = '';
-  //     $dbh = new PDO($dsn, $user, $password);
-  //     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  //     return $dbh;
-  //   } else {
-  $dsn = 'mysql:dbname=heroku_30e4f1b3d09aef2;host=us-cdbr-east-05.cleardb.net;charset=utf8';
-  $user = 'bcf7f8bfec19ab';
-  $password = 'dfb3857b';
-  $dbh = new PDO($dsn, $user, $password);
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  return $dbh;
-  // }
+  if ($_SERVER["HTTP_HOST"] == "localhost") {
+    $dsn = 'mysql:dbname=chat;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+  } else {
+    $dsn = 'mysql:dbname=heroku_30e4f1b3d09aef2;host=us-cdbr-east-05.cleardb.net;charset=utf8';
+    $user = 'bcf7f8bfec19ab';
+    $password = 'dfb3857b';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    return $dbh;
+  }
 }
