c75b00f02ff713d8cb791b161b702060875a3e20
karirin <karirin419@gmail.com>
2022-09-13 19:35:41 +0900

    'profile.phpの設定をログイン時のみに'

--- footer.php
+++ footer.php
@@ -1,6 +1,8 @@
 <?php
 require('testcase_process.php');
-require('profile.php');
+if (isset($_SESSION['login']) == true) {
+    require('profile.php');
+}
 ?>
 <script src=" https://code.jquery.com/jquery-3.4.1.min.js "></script>
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
