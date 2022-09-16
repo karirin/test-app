eb18d780ac52c67bd322d93877b878b681dd3cb3
karirin <karirin419@gmail.com>
2022-09-16 23:27:50 +0900

    'db環境設定更新'

--- db_connect.php
+++ db_connect.php
@@ -9,9 +9,9 @@ function db_connect()
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
   } else {
-    $dsn = 'mysql:dbname=heroku_2bba4f302b3d09f;host=us-cdbr-east-06.cleardb.net;charset=utf8';
-    $user = 'b0fc2b38303676';
-    $password = '803907cf';
+    $dsn = 'mysql:dbname=heroku_e2fdebc6d9c4e72;host=us-cdbr-east-06.cleardb.net;charset=utf8';
+    $user = 'b1cb04accddbd8';
+    $password = '62537c22';
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
