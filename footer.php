8fc0188a7e059dce2e36551ddcf7cab62dc2865d
karirin <karirin419@gmail.com>
2022-09-18 10:32:50 +0900

    '本番環境のDB設定修正'

--- footer.php
+++ footer.php
@@ -1,12 +1,14 @@
 <?php
-if (isset($_SESSION['login']) == true) {
+if (isset($_SESSION['login']) == true) :
     require('profile.php');
     require('post_process.php');
     require('testcase_process.php');
-}
 ?>
 <script src=" https://code.jquery.com/jquery-3.4.1.min.js "></script>
 <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
 <script src="/js/common.js"></script>
 <script src="/js/test.js"></script>
+<?php
+endif;
+?>
--- user/user_add.php
+++ user/user_add.php
@@ -32,6 +32,10 @@ require_once('../config_1.php');
                 </div>
             </form>
 </body>
-<?php require_once('../footer.php'); ?>
+<script src=" https://code.jquery.com/jquery-3.4.1.min.js "></script>
+<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.12.0/jquery-ui.min.js"></script>
+<script src="/js/common.js"></script>
+<script src="/js/test.js"></script>
 
 </html>
